29012675675cfe18b0af96ab9f0d1a9a0ea04cec
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2025-01-13 17:52:56 +0100

    Add files via upload

--- utils/config.py
+++ utils/config.py
@@ -1,12 +1,10 @@
 import mysql.connector
 import streamlit as st
 
-try:
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="gacep"
-    )
-except:
-    mydb = st.error("ERRORE CONNESSIONE DB")
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="gacep"
+)
